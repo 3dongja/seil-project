94202585cd3e42b26fb62a9091ba4eefbfc289d3
3dongja <jinhyung861009@gmail.com>
2025-06-17 00:35:00 +0900

    수정3

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,8 +1,15 @@
+// lib/firebase-admin.ts
+
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY!);
+
+  // 환경변수 내 private_key 문자열의 이스케이프된 \\n을 실제 줄바꿈 문자 \n 으로 변환
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
   admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_ADMIN_KEY!))
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
