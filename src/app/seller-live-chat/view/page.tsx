7bdf8c578b4f4d1b08f5b0e4093dcf3c13349c5d
3dongja <jinhyung861009@gmail.com>
2025-06-22 20:52:37 +0900

    수정8

--- src/app/seller-live-chat/view/page.tsx
+++ src/app/seller-live-chat/view/page.tsx
@@ -2,15 +2,10 @@
 "use client";
 export const dynamic = "force-dynamic";
 
-import { useSearchParams } from "next/navigation";
-import SellerLiveChatPage from "../SellerLiveChatPage";
 import { Suspense } from "react";
+import SellerLiveChatPage from "../SellerLiveChatPage";
 
-export default function ChatViewRoute() {
-  const searchParams = useSearchParams();
-  const sellerId = searchParams.get("seller") || "";
-  const inquiryId = searchParams.get("inquiry") || "";
-
+export default function Page() {
   return (
     <Suspense fallback={<div>로딩 중...</div>}>
       <SellerLiveChatPage />
