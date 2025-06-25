242d940eec529879d048958fce89da96fd8a79a3
3dongja <jinhyung861009@gmail.com>
2025-06-26 01:10:28 +0900

    챗봇 8

--- src/app/chat-summary/[sellerId]/[inquiryId]/bot/page.tsx
+++ src/app/chat-summary/[sellerId]/[inquiryId]/bot/page.tsx
@@ -1,10 +1,20 @@
 import ChatBotScreen from "@/components/chat/ChatBotScreen";
 import { notFound } from "next/navigation";
+import { FC } from "react";
 
-export default async function Page({ params }: { params: { sellerId: string; inquiryId: string } }) {
+interface Props {
+  params: {
+    sellerId: string;
+    inquiryId: string;
+  };
+}
+
+const Page: FC<Props> = async ({ params }) => {
   const { sellerId, inquiryId } = params;
 
   if (!sellerId || !inquiryId) return notFound();
 
   return <ChatBotScreen sellerId={sellerId} inquiryId={inquiryId} />;
-}
+};
+
+export default Page;
